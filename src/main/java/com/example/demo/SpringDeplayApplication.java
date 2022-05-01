597f2aa630b1cd46653df2866f827db50a41e46c
toich81 <96623509+toich81@users.noreply.github.com>
2022-05-01 17:02:15 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/SpringDeplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDeplayApplication.class, args);
+	}
+
+}
